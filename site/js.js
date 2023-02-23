8952ce03f299b3583d988ebdeb0c2ed18515a397
JOÃO VÍTOR <125417384+Irregular-JV@users.noreply.github.com>
2023-02-22 21:57:41 -0300

    NAV REOGANIZADA

---
+++ site/js.js
@@ -0,0 +1,7 @@
+function canal() {
+        // Exibe uma caixa de diálogo modal para fazer uma pergunta ao usuário
+        var answer = confirm("Você quer ir para o canal no You Tube?");
+        // Se ele clicou no botão "OK", faz o navegador carregar uma nova página
+        if (answer) window.location = "https://www.youtube.com/channel/UChPgKjcJAA2vWk6lKzCuDvw";
+       }
+       
